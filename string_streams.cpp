55e59b3f88674cf2ed6d2bef76e215b70ea77893
iamna <i.am.nadim95@gmail.cojm>
2024-01-21 02:04:36 +0200

    Lab 2 done

---
+++ string_streams.cpp
@@ -0,0 +1,63 @@
+/*
+ * Write a program that asks user to enter a line that contains integers or “stop” to stop the program. If
+ * user enters a string that starts with “stop” the program stops. Otherwise, the program creates a
+ * string stream from the user input and reads integers from the stream. Program keeps reading and
+ * summing integer until the stream ends or reading an integer fails because text or other nonnumerical character is encountered.
+ * When the reading stops the sum and count are printed and program asks user to enter another string.
+ * For example:
+ * Enter a line with numbers or “stop”: 1 6 34 78 4 6
+ * Total of 6 numbers is 129
+ * Enter a line with numbers or “stop”: 4 99 zip 44 6
+ * Total of 2 numbers is 103
+ * Enter a line with numbers or “stop”: stop
+ */
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <numeric>
+
+int main() {
+    std::string line;
+    while (true){
+        std::cout << "Enter a line with numbers or \"stop\": ";
+        std::getline(std::cin, line);
+        if (line.find("stop") == 0){
+            std::cout << "Program has been stopped !\n";
+            break;
+        }
+        std::istringstream stream(line);
+        int sum = 0;
+        int count = 0;
+        int number;
+        while (stream >> number){
+            sum += number;
+            count++;
+        }
+        std::cout << "Total of " << count << " numbers is " << sum << std::endl;
+    }
+    return 0;
+}
+
+/*
+ int main() {
+    std::string line;
+    std::vector<int> numbers;
+    int pts;
+    while (true){
+        std::cout << "Enter a line with numbers or \"stop\": ";
+        std::getline(std::cin, line);
+        if (line.find("stop") == 0){
+            std::cout << "Program has been stopped !\n";
+            break;
+        }
+        std::istringstream stream(line);
+        while (stream >> pts){
+            numbers.push_back(pts);
+        }
+        std::cout << "Total of " << numbers.size() << " numbers is " << std::accumulate(numbers.begin(), numbers.end(), 0) << std::endl;
+    }
+    return 0;
+}
+
+ */
---
+++ using_reference_parameter.cpp
@@ -0,0 +1,56 @@
+/*
+ * Write a function that takes a reference to string and a character (by value) as parameters. The
+ * function replaces all occurrences of the character with underscores.
+ * Write a program that ask user to enter a string. Then program asks user to enter character to replace
+ * or “stop” to stop the program. Note that program must read a string after the second question to be
+ * able to test for “stop”. If answer is not “stop” then program passes the first character of second
+ * question’s answer as the character to replace. Then program prints the string and continues. If
+ * answer is “stop” the program ends.
+ * For example:
+ * Enter a string: This is a test
+ * Enter character to replace or “stop”: i
+ * Result: Th_s _s a test
+ * Enter a string: I love C++
+ * Enter character to replace or “stop”: stop
+ */
+
+#include <iostream>
+#include <string>
+
+void replace_character(std::string &str, char character){
+
+    for (char & i : str){
+        if (i == character){
+            i = '_';
+        }
+    }
+    /*
+    std::string replaceWith {"_"};
+    size_t pos = str.find(character);
+    while (pos != std::string::npos){
+        str.replace(pos, replaceWith.size(), replaceWith);
+        pos = str.find(character);
+    }
+     */
+
+
+}
+
+
+int main() {
+    std::string str;
+    std::string character;
+    while (true){
+        std::cout << "Enter a string: ";
+        std::getline(std::cin, str);
+        std::cout << "Enter character to replace or \"stop\" : ";
+        std::getline(std::cin, character);
+       if(!character.compare("stop")){
+           std::cout << "\nProgram has been stopped !\n";
+            break;
+       }
+        replace_character(str, character[0]);
+        std::cout << "Result: " << str << std::endl;
+    }
+    return 0;
+}
